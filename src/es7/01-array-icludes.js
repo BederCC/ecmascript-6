35aec6faebe115204c3e22cd75089f1ad0c31b25
BederCC <beder1810@gmail.com>
2025-01-05 22:07:03 -0500

    includes con array

---
+++ src/es7/01-array-icludes.js
@@ -0,0 +1,9 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers.includes(7)); // true
+
+const list = ['apple', 'banana', 'kiwi', 'orange', 'mango'];
+
+console.log(list.includes('Apple')); // false por la mayuscula
+console.log(list.includes('apple')); // true
+

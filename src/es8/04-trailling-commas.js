6e1001e5246287f28554081fad0eaffa13e70bd4
BederCC <beder1810@gmail.com>
2025-01-05 22:24:58 -0500

    comas en un array

---
+++ src/es8/04-trailling-commas.js
@@ -0,0 +1,5 @@
+// esto sirve para que no de error si se deja una coma al final de un arreglo o un objeto
+const numbers = [24, 34, 25, 24, , , , 45];
+
+console.log(numbers);
+console.log(numbers.length);

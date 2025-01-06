4f01088e2a87a58ff7d6e5d5a514bda5035314fc
BederCC <beder1810@gmail.com>
2025-01-05 22:14:14 -0500

    object con entries

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,5 @@
+const continents = {MX: 'mexico', CO: 'colombia', AR: 'argentina', BR: 'brasil', PE: 'peru'};
+console.log(Object.values(continents));
+console.log(Object.keys(continents));
+
+console.log(Object.entries(continents));

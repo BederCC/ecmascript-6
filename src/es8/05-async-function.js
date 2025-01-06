ebb47f417cf54990cd866b6d9fb8a7d9467f3b31
BederCC <beder1810@gmail.com>
2025-01-05 22:38:56 -0500

    async y await

---
+++ src/es8/05-async-function.js
@@ -0,0 +1,17 @@
+const fnAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(() => resolve('Async Hello World'), 5000)
+        : reject(new Error('Error'))
+    })
+};
+
+const anotherFn = async () => {
+    const something = await fnAsync();
+    console.log(something);
+    console.log('Another function');
+}
+
+console.log('Before');
+anotherFn();
+console.log('After');

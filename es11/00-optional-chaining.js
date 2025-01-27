5ca70b1a30a7d4af48e08df489ce16beafa959c6
BederCC <beder1810@gmail.com>
2025-01-27 00:00:35 -0500

    chaining

---
+++ es11/00-optional-chaining.js
@@ -0,0 +1,12 @@
+const users = {
+    gndx:{
+        country : 'MX',
+    },
+    beder:{
+        country : 'AR',
+    }
+}
+
+console.log(users.gndx.age)
+
+console.log(users.bebeloper?.country);

c3c2537817b33c1468234438acdfda31bde54e1f
BederCC <beder1810@gmail.com>
2025-02-12 22:40:56 -0500

    separador de numeros y reemplazar plalabras

---
+++ es12/01-replaceall.js
@@ -0,0 +1,3 @@
+const string = "JavaScript is a programming language. JavaScript is a scripting language.";
+const replacedString = string.replaceAll("JavaScript", "JS");
+console.log(replacedString); // JS is a programming language. JS is a scripting language.
